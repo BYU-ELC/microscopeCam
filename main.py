5b6be066abb8badfddab97004537465fbb1989d0
ELC Shop Techies <ecenshop@gmail.com>
2021-07-21 16:13:06 -0600

    fullscreen?

--- main.py
+++ main.py
@@ -16,6 +16,9 @@ class Timer:
 
 
 vs = cv2.VideoCapture(0)
+vs.set(CAP_PROP_FRAME_WIDTH, 1280)
+vs.set(CAP_PROP_FRAME_HEIGHT, 720)
+
 clock = Timer()
 
 button = Button(14)
@@ -26,27 +29,34 @@ font = cv2.FONT_HERSHEY_SIMPLEX
 org = (50, 50)
 fontScale = 1
 textColor = (255, 0, 255)
+window_name = "Microscope"
+cv2.namedWindow(window_name, cv2.WND_PROP_FULLSCREEN)
+cv2.setWindowProperty(window_name, cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
   
 # Line thickness of 2 px
 thickness = 2
 
 while True:
     print(clock.get())
-    timeLeft = end - round(clock.get());
+    timeLeft = end - round(clock.get())
 
     ret, frame = vs.read()
     # frame = cv2.flip(frame, 1)
 
     if (timeLeft <= 30):
-        cv2.putText(frame, str(timeLeft), org, font, fontScale, textColor, thickness, cv2.LINE_AA)
+        cv2.putText(frame, "Shutting down in: " + str(timeLeft), org, font, fontScale, textColor, thickness, cv2.LINE_AA)
 
-    cv2.imshow("camera", frame)
+    cv2.imshow(window_name, frame)
 
     k = cv2.waitKey(1)
     if k == ord('r') or (button.is_pressed):
-        clock.reset();
+        clock.reset()
     if k == ord('q'):
         break
     if timeLeft <= 0:
         break
 
+
+# cleanup
+vs.release()
+cv2.destroyAllWindows()
