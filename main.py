e39624569ee965d01b2d44654499f206510530a5
Casey <ecenshop@gmail.com>
2021-07-29 14:00:31 -0600

    make button work

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import cv2
 from cv2 import VideoCapture
 import time
 
-from gpiozero import Button
+import RPi.GPIO as GPIO
  
 class Timer:
     def __init__(self):
@@ -17,12 +17,29 @@ class Timer:
 
 
 vs = cv2.VideoCapture(0)
-vs.set(cv2.CAP_PROP_FRAME_WIDTH, 1920)
-vs.set(cv2.CAP_PROP_FRAME_HEIGHT, 1440)
+# vs.set(cv2.CAP_PROP_FRAME_WIDTH, 1920)
+# vs.set(cv2.CAP_PROP_FRAME_HEIGHT, 1440)
+
+vs.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
+vs.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
+
+
+
 
 clock = Timer()
 
-button = Button(14)
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(14, GPIO.IN, pull_up_down=GPIO.PUD_UP) # use internal pull up
+'''
+Oh, the grand old Duke of York
+He had ten thousand men
+He marched them up to the top of the hill
+And he marched them down again
+And when they were up, they were up
+And when they were down, they were down
+And when they were only half-way up
+They were neither up nor down
+'''
 
 end = 600 # time in seconds to run
 
@@ -30,29 +47,35 @@ font = cv2.FONT_HERSHEY_SIMPLEX
 org = (50, 50)
 fontScale = 1
 textColor = (255, 0, 255)
+thickness = 2
 window_name = "Microscope"
 cv2.namedWindow(window_name, cv2.WND_PROP_FULLSCREEN)
 cv2.setWindowProperty(window_name, cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
 
-# Line thickness of 2 px
-thickness = 2
+credits = True;
 
 while True:
     # print(clock.get())
     timeLeft = end - round(clock.get())
 
     ret, frame = vs.read()
+    frame = cv2.resize(frame,(1920,1440),fx=0,fy=0, interpolation = cv2.INTER_CUBIC)
     # frame = cv2.flip(frame, 1)
+
+    if (timeLeft > (end - 3)) and credits:
+        cv2.putText(frame, "Made by Casey", org, font, fontScale, textColor, thickness, cv2.LINE_AA)
 
     if (timeLeft <= 30):
         cv2.putText(frame, "Shutting down in: " + str(timeLeft), org, font, fontScale, textColor, thickness, cv2.LINE_AA)
+        cv2.putText(frame, "Smash the big red button on the right of the table to reset the timer", (50, 80), font, fontScale, textColor, thickness, cv2.LINE_AA)
 
     cv2.imshow(window_name, frame)
 
     k = cv2.waitKey(1)
-    if k == ord('r') or (button.is_pressed):
+    if (k == ord('r')) or (GPIO.input(14)):
     # if k == ord('r'):
         clock.reset()
+        credits = False
     if k == ord('q'):
         break
     if timeLeft <= 0:
